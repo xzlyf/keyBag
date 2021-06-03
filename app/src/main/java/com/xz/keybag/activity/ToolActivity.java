30f8905ac0e054e752aa73a6dcb779db7e7e398d
xzlyf <czr2001@outlook.com>
2021-06-03 21:03:37 +0800

    DES和RSA加入 alpha分支

--- app/src/main/java/com/xz/keybag/activity/ToolActivity.java
+++ app/src/main/java/com/xz/keybag/activity/ToolActivity.java
@@ -7,14 +7,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.xz.keybag.R;
 import com.xz.keybag.base.BaseActivity;
+import com.xz.keybag.custom.UnifyEditView;
+import com.xz.keybag.utils.lock.DES;
+import com.xz.keybag.utils.lock.RSA;
 import com.xz.keybag.utils.lock.SHA256;
 import com.xz.utils.CopyUtil;
+
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -29,9 +35,24 @@ public class ToolActivity extends BaseActivity {
 	TextView tvPre;
 	@BindView(R.id.spinner)
 	Spinner spinner;
+	@BindView(R.id.des_layout)
+	LinearLayout desLayout;
+	@BindView(R.id.ue_des)
+	UnifyEditView ueDes;
+	@BindView(R.id.tv_des)
+	TextView tvDes;
+	@BindView(R.id.rsa_layout)
+	LinearLayout rsaLayout;
+	@BindView(R.id.ue_rsa_public)
+	UnifyEditView ueRsaPublic;
+	@BindView(R.id.ue_rsa_private)
+	UnifyEditView ueRsaPrivate;
+	@BindView(R.id.tv_rsa)
+	TextView tvRsa;
 
 	private CopyUtil copyUtil;
 	private String[] algorithmArray = {"SHA256", "DES", "RSA"};
+	private int type; //加密模式 0-sha256 1-des 2-rsa
 
 	@Override
 	public boolean homeAsUpEnabled() {
@@ -47,8 +68,8 @@ public class ToolActivity extends BaseActivity {
 	public void initData() {
 		changeStatusBarTextColor();
 		copyUtil = new CopyUtil(this);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, algorithmArray);
-		spinner.setAdapter(adapter);
+		nonType();
+		spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, algorithmArray));
 		etSource.addTextChangedListener(new TextWatcher() {
 			@Override
 			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -68,8 +89,20 @@ public class ToolActivity extends BaseActivity {
 		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				if (position == 1) {
-
+				nonType();
+				switch (position) {
+					case 0:
+						shaType();
+						break;
+					case 1:
+						desType();
+						break;
+					case 2:
+						rsaType();
+						break;
+					default:
+						nonType();
+						break;
 				}
 			}
 
@@ -81,7 +114,8 @@ public class ToolActivity extends BaseActivity {
 	}
 
 
-	@OnClick({R.id.tv_back, R.id.tv_make, R.id.tv_copy, R.id.tv_paste, R.id.tv_copy_clip})
+	@OnClick({R.id.tv_back, R.id.tv_make, R.id.tv_copy, R.id.tv_paste, R.id.tv_copy_clip
+			, R.id.tv_des, R.id.tv_rsa})
 	public void onViewClicked(View view) {
 		switch (view.getId()) {
 			case R.id.tv_back:
@@ -102,6 +136,16 @@ public class ToolActivity extends BaseActivity {
 				copyUtil.copyToClicp(tvPre.getText().toString());
 				Snackbar.make(view, "已复制", Snackbar.LENGTH_SHORT).show();
 				break;
+			case R.id.tv_des:
+				ueDes.setText(DES.getKey());
+				break;
+			case R.id.tv_rsa:
+				Map<String, String> keys = RSA.createKeys(1024);
+				if (keys != null) {
+					ueRsaPublic.setText(keys.get("publicKey"));
+					ueRsaPrivate.setText(keys.get("privateKey"));
+				}
+				break;
 		}
 	}
 
@@ -109,6 +153,84 @@ public class ToolActivity extends BaseActivity {
 		String st = etSource.getText().toString();
 		if (TextUtils.isEmpty(st))
 			return;
-		tvPre.setText(SHA256.getSHA256(st));
+		String tar = null;
+		switch (type) {
+			case 0:
+				//sha256
+				tar = SHA256.getSHA256(st);
+				break;
+			case 1:
+				//des
+				String secret = ueDes.getText().toString().trim();
+				if (TextUtils.isEmpty(secret)) {
+					sToast("请先输入一个密钥");
+					return;
+				}
+				try {
+					tar = DES.encryptor(st, secret);
+				} catch (Exception e) {
+					tar = e.getMessage();
+				}
+				break;
+			case 2:
+				//rsa
+				String publicKey = ueRsaPublic.getText().toString().trim();
+				String privateKey = ueRsaPrivate.getText().toString().trim();
+				if (TextUtils.isEmpty(publicKey)) {
+					sToast("请先输入一个公钥");
+					return;
+				}
+				if (TextUtils.isEmpty(privateKey)) {
+					sToast("请先输入一个私钥");
+					return;
+				}
+				try {
+					tar = RSA.publicEncrypt(st, RSA.getPublicKey(publicKey));
+				} catch (Exception e) {
+					tar = e.getMessage();
+				}
+				break;
+		}
+		tvPre.setText(tar);
+	}
+
+	/**
+	 * 不选择任何模式
+	 */
+	private void nonType() {
+		type = -1;
+		desLayout.setVisibility(View.GONE);
+		rsaLayout.setVisibility(View.GONE);
+	}
+
+	/**
+	 * sha256模式
+	 */
+	private void shaType() {
+		type = 0;
+
+	}
+
+	/**
+	 * des模式
+	 */
+	private void desType() {
+		type = 1;
+		desLayout.setVisibility(View.VISIBLE);
+		ueDes.setText(DES.getKey());
+
+	}
+
+	/**
+	 * rsa模式
+	 */
+	private void rsaType() {
+		type = 2;
+		rsaLayout.setVisibility(View.VISIBLE);
+		Map<String, String> keys = RSA.createKeys(512);
+		if (keys != null) {
+			ueRsaPublic.setText(keys.get("publicKey"));
+			ueRsaPrivate.setText(keys.get("privateKey"));
+		}
 	}
 }
